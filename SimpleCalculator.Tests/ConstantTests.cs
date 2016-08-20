e7f07a4c6350f1848d8f73d8ec74023fb8cff57a
MATT BRUTON <the.bruton@gmail.com>
2016-08-20 11:50:11 -0500

    added 2 tests for constants class

---
+++ SimpleCalculator.Tests/ConstantTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SimpleCalculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Tests
+{
+    [TestClass]
+    public class ConstantTests
+    {
+        [TestMethod]
+        public void ConstantIsInputANewConstReturnsTrueIfNewConst()
+        {
+            Assert.AreEqual(true, Constants.IsInputANewConst("n = 2"));
+        }
+
+        [TestMethod]
+        public void ConstantIsInputANewConstReturnsFalseIfInputDoesNotMatchPattern()
+        {
+            Assert.AreEqual(false, Constants.IsInputANewConst("n + 2"));
+        }
+    }
+}
