e517cef560f91b4371008ddfe897bec7b109e445
MATT BRUTON <the.bruton@gmail.com>
2016-08-20 11:04:27 -0500

    added subtraction and multiplication unit tests

--- SimpleCalculator.Tests/MultiplyTests.cs
+++ SimpleCalculator.Tests/MultiplyTests.cs
@@ -5,25 +5,25 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace SimpleCalculator.Tests
 {
     [TestClass]
-    public class AdditionTests
+    public class MultiplyTests
     {
         [TestMethod]
-        public void AddCanBeInstantiated()
+        public void MultiplyCanBeInstantiated()
         {
-            Add test = new Add();
+            Multiply test = new Multiply();
 
             Assert.IsNotNull(test);
         }
 
         [TestMethod]
-        public void AddOperateGivesExpectedOutputWithValidInput()
+        public void MultiplyOperateGivesExpectedOutputWithValidInput()
         {
-            Add test = new Add();
+            Multiply test = new Multiply();
 
             Assert.AreEqual("4", test.Operate(2, 2));
-            Assert.AreEqual("10", test.Operate(8, 2));
-            Assert.AreEqual("205", test.Operate(200, 5));
-            Assert.AreEqual("1", test.Operate(-2, 3));
+            Assert.AreEqual("16", test.Operate(8, 2));
+            Assert.AreEqual("1000", test.Operate(200, 5));
+            Assert.AreEqual("-6", test.Operate(-2, 3));
             Assert.AreEqual("0", test.Operate(0, 0));
         }
     }
---
+++ SimpleCalculator.Tests/SubtractionTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SimpleCalculator.Operations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Tests
+{
+    [TestClass]
+    public class SubtractionTests
+    {
+        [TestMethod]
+        public void SubtractCanBeInstantiated()
+        {
+            Subtract test = new Subtract();
+
+            Assert.IsNotNull(test);
+        }
+
+        [TestMethod]
+        public void SubtractOperateGivesExpectedOutputWithValidInput()
+        {
+            Subtract test = new Subtract();
+
+            Assert.AreEqual("0", test.Operate(2, 2));
+            Assert.AreEqual("6", test.Operate(8, 2));
+            Assert.AreEqual("195", test.Operate(200, 5));
+            Assert.AreEqual("-5", test.Operate(-2, 3));
+            Assert.AreEqual("0", test.Operate(0, 0));
+        }
+    }
+}
